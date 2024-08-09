8d37020306d3b54b1152922690768e9f8d787aae
Simon Hampel <simon@hampelgroup.com>
2024-08-09 21:40:09 +1000

    add options to set transactional / click tracking / open tracking

--- src/Mime/SparkPostEmail.php
+++ src/Mime/SparkPostEmail.php
@@ -67,6 +67,27 @@ class SparkPostEmail extends Email
         return $this;
     }
 
+    public function setTransactional(bool $transactional = true): SparkPostEmail
+    {
+        $this->options['transactional'] = $transactional;
+
+        return $this;
+    }
+
+    public function setClickTracking(bool $clickTracking = true): SparkPostEmail
+    {
+        $this->options['click_tracking'] = $clickTracking;
+
+        return $this;
+    }
+
+    public function setOpenTracking(bool $openTracking = true): SparkPostEmail
+    {
+        $this->options['open_tracking'] = $openTracking;
+
+        return $this;
+    }
+
     public function getContent(): ?array
     {
         return $this->content;
